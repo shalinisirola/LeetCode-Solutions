5934d30218f22f5b2dd22521704a31535701041a
Shalini Singh Sirola <159011436+shalinisirola@users.noreply.github.com>
2025-11-26 12:51:14 +0530

    Update solution.cpp

--- Arrays/15_3Sum/solution.cpp
+++ Arrays/15_3Sum/solution.cpp
@@ -2,29 +2,39 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> output;
-        sort(nums.begin(),  nums.end());
         int n = nums.size();
-        for(int i=0; i<n; i++){
-            int j=i+1;
-            int k=n-1;
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum<0){
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
                     j++;
                 }
-                else if(sum>0){
+                else if (sum > 0) {
                     k--;
                 }
-                else{
+                else {
                     output.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                } 
-                while(j<k && nums[j]==nums[j+1]) j++;
 
+                    
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+
+                    
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
             }
         }
+
         return output;
     }
 };
+
