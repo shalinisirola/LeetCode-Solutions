ebbcdc83b87c9ec677c343ede88f936ed957608a
Shalini Singh Sirola <159011436+shalinisirola@users.noreply.github.com>
2026-01-15 09:26:58 +0530

    Update solution.cpp

--- Arrays/268. Missing Number/solution.cpp
+++ Arrays/268. Missing Number/solution.cpp
@@ -12,3 +12,18 @@ public:
         return XOR1^XOR2;
     }
 };
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         int XOR1=0, XOR2=0;
+//         for(int i=0; i<n; i++){
+//             XOR1=XOR1^i;
+//             XOR2=XOR2^(nums[i]);
+//         }
+//         XOR1=XOR1^n;
+        
+//         return XOR1^XOR2;
+//     }
+// };
