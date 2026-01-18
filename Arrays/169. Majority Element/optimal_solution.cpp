42bf4a990471fabe707c0fae4a7f0457d4e5cf0d
Shalini Singh Sirola <159011436+shalinisirola@users.noreply.github.com>
2026-01-19 00:11:33 +0530

    Create optimal_solution.cpp

---
+++ Arrays/169. Majority Element/optimal_solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count = 1;
+        int el = nums[0];
+        for(int i=1; i<n; i++){
+            if(count==0){
+                el = nums[i];
+            }
+            if(nums[i]==el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int count_el=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==el) count_el++;
+        }
+        if(count_el > n/2){
+            return el;
+        }
+        return -1;
+    }
+};
